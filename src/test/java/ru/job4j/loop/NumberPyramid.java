f77737093e15a9e58b364b71b6dfb49d95151151
blanksearch <blankinsearch@gmail.com>
2025-07-13 16:11:58 +0200

    Числовая пирамида [#505218]

---
+++ src/test/java/ru/job4j/loop/NumberPyramid.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+public class NumberPyramid {
+    public static void draw(int n) {
+        for (int i = 1; i <= n; i++) {
+            // Печатаем пробелы
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            // Печатаем числа в прямом порядке
+            for (int k = 1; k <= i ; k++) {
+                System.out.print(k);
+            }
+            // Печатаем числа в обратном порядке
+            for (int t = i - 1; t > 0; t--) {
+                System.out.print(t);
+            }
+            // Переход на новую строку
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Number pyramid of height 3:");
+        draw(3);
+        System.out.println("Number pyramid of height 5:");
+        draw(5);
+    }
+}
