12c534cb7d7678cbe72ec55f6d9be99596f1b026
blanksearch <blankinsearch@gmail.com>
2025-07-14 09:57:48 +0200

    [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,18 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Adam Mickiewicz";
+        names[1] = "Olga Tokarczuk";
+        names[2] = "Henryk Sinkiewicz";
+        names[3] = "Julian Tuwim";
+
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
