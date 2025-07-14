4757f2b6ab43fe93dc6a535046bbdcbbf04102c5
blanksearch <blankinsearch@gmail.com>
2025-07-14 09:31:42 +0200

    [#505215 #548203] fixed

--- src/main/java/ru/job4j/loop/PrimeNumber.java
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -5,7 +5,9 @@ public class PrimeNumber {
         int count = 0;
 
         for (int i = 2; i <= finish; i++) {
-            count =  CheckPrimeNumber.check(i) ? 1 : 0;
+            if (CheckPrimeNumber.check(i)) {
+                count++;
+            }
         }
         return count;
     }
