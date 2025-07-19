ebe1323a5a1329d33b4b327ac16243d6c7591548
blanksearch <blankinsearch@gmail.com>
2025-07-19 13:24:39 +0200

    [#122643]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -40,4 +40,40 @@ class PointTest {
         double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
+
+    @Test
+    void whenPoints000And111Then1Dot73() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(1, 1, 1);
+        double expected = 1.73;
+        double result = a.distance3d(b);
+        assertThat(result).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenPoints123And321Then3Dot46() {
+        Point a = new Point(1, 2, 3);
+        Point b = new Point(3, 2, 1);
+        double expected = 2.83;
+        double result = a.distance3d(b);
+        assertThat(result).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenPointsMinus100And100Then2() {
+        Point a = new Point(-1, 0, 0);
+        Point b = new Point(1, 0, 0);
+        double expected = 2;
+        double result = a.distance3d(b);
+        assertThat(result).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenSamePointsThen0() {
+        Point a = new Point(3, -5, 7);
+        Point b = new Point(3, -5, 7);
+        double expected = 0;
+        double result = a.distance3d(b);
+        assertThat(result).isEqualTo(expected, withPrecision(0.01));
+    }
 }
